ee31340ad2f3a3ec10ada8e780f5c8794b1daa46
Piotr Bąk <piotr.andrzej.bak@gmail.com>
2022-03-21 02:08:48 +0100

    r0.01

---
+++ MaroonZincWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MaroonZincWebApp.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace MaroonZincWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
---
+++ MaroonZincWebApp/Model/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MaroonZincWebApp.Model
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+    }
+}
